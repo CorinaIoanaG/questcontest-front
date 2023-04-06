0c7381b9a435be8b8d183f61da47f656b6fae9d9
CorinaIoanaG <corinaghete@gnail.com>
2023-04-06 12:51:53 +0300

    First Commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,16 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
+import { Box, Typography } from '@mui/material';
+
+axios.get('https://localhost:8080/quest');
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Box className='App' sx={{ backgroundColor: 'lightgray', height: 2, display: "flex", flexDirection: "column"}}>
+    <Typography sx ={{height: 50}} fontSize={"large"}> QUEST CONTEST </Typography>
+    </Box>
   );
 }
 
